b6a9276c9a2e3621395503134b12944107be6262
SebastianOderland <sebastian.oderland@gmail>
2021-03-17 14:26:06 +0100

    update

--- create_files_and_git.sh
+++ create_files_and_git.sh
@@ -3,6 +3,7 @@ update_description=$1
 # COPY AND ZIP
 mkdir plugin
 cp update_primary_domain.pl plugin/update_primary_domain.pl
+cp index.html plugin/index.html
 tar -czvf plugin/update_primary_domain.tar.gz update_primary_domain
 tar -czvf plugin.tar.gz plugin
 rm -rf plugin
--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ curl -s https://raw.githubusercontent.com/SebastianOderland/update_primary_domai
 
 tar -xvzf /root/tmp/plugin.tar.gz --directory /root/tmp;
 
+
 mv -v /root/tmp/plugin/* /usr/local/cpanel/base/frontend/paper_lantern/update_primary_domain/
 mkdir -p /usr/local/cpanel/base/frontend/paper_lantern/update_primary_domain
 /usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/paper_lantern/update_primary_domain/update_primary_domain.tar.gz
@@ -20,3 +21,5 @@ rm -rf /root/tmp/plugin
 """
 
 echo "Installation is complete!"
+
+#rm /usr/local/cpanel/base/frontend/paper_lantern/update_primary_domain/
