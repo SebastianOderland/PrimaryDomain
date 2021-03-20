83be7772874a1f8ef6fbeb8587d9359026cd35a6
SebastianOderland <sebastian.oderland@gmail.com>
2021-03-20 14:30:09 +0100

    update

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ ssh ${SERVER} """
 
 echo "Installing [Change Primary Domain] Plugin"
 
-curl -s https://raw.githubusercontent.com/SebastianOderland/update_primary_domain/main/plugin.tar.gz > /root/tmp/change_primary_domain.tar.gz;
+curl -s https://raw.githubusercontent.com/SebastianOderland/update_primary_domain/main/plugin.tar.gz > /root/tmp/plugin_files.tar.gz;
 
-tar -xvzf /root/tmp/change_primary_domain.tar.gz --directory /root/tmp;
+tar -xvzf /root/tmp/plugin_files.tar.gz --directory /root/tmp;
 
 
-mv -v /root/tmp/change_primary_domain/* /usr/local/cpanel/base/frontend/paper_lantern/change_primary_domain/
 mkdir -p /usr/local/cpanel/base/frontend/paper_lantern/change_primary_domain
+mv -v /root/tmp/plugin_files/* /usr/local/cpanel/base/frontend/paper_lantern/change_primary_domain/
 /usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/paper_lantern/change_primary_domain/change_primary_domain.tar.gz
 
 """
--- push.sh
+++ push.sh
@@ -1,11 +1,11 @@
 update_description=$1
 
 # COPY AND ZIP
-mkdir plugin_compressed
-cp -R plugin/. plugin_compressed/
-tar -czvf plugin_compressed/change_primary_domain.tar.gz change_primary_domain
-tar -czvf plugin.tar.gz plugin_compressed
-rm -rf plugin_compressed
+mkdir plugin_files
+cp -R plugin/. plugin_files/
+tar -czvf plugin_files/change_primary_domain.tar.gz change_primary_domain
+tar -czvf plugin.tar.gz plugin_files
+rm -rf plugin_files
 
 # GIT
 git add .
